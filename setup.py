76191a7b6e860ef50d37ddb9f54b9755318b95d2
Nico Schlömer <nico.schloemer@gmail.com>
2012-03-01 17:44:46 +0100

    improved setup.py

--- setup.py
+++ setup.py
@@ -19,17 +19,30 @@
 # along with VoroPy.  If not, see <http://www.gnu.org/licenses/>.
 #
 # ==============================================================================
-from distutils.core import setup
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup( name='voropy',
        version='0.0.1',
-       packages=['voropy'],
+       author = 'Nico Schlömer',
+       author_email = 'nico.schloemer@gmail.com',
+       packages=['voropy', 'tests'],
+       description = 'Delaunay meshes, Voronoi regions',
+       long_description=read('README'),
        url = 'https://bitbucket.org/nschloe/voropy',
        download_url='https://bitbucket.org/nschloe/voropy/downloads',
-       author = 'Nico Schl"omer',
-       author_email = 'nico.schloemer@gmail.com',
-       description = 'Delaunay meshes, Voronoi regions',
        license = 'GNU Lesser General Public License (LGPL), Version 3',
        platforms='any',
-       requires=['numpy','scipy','vtk']
+       requires=['numpy','scipy','vtk'],
+       classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities'
+        ],
      )
