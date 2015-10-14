b8509f6f555c0b0c21298e50793341264e793e86
Nico Schlömer <nico.schloemer@gmail.com>
2015-10-14 16:00:18 +0200

    docs: hardcode author name

--- doc/conf.py
+++ doc/conf.py
@@ -45,8 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'VoroPy'
-from voropy import __author__
-copyright = u'2015, ' + __author__
+copyright = u'2015, Nico Schlömer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
