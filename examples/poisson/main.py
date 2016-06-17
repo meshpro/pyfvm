64aa0f6ed229990f4d723c57bf900ca25df0aca9
Nico Schlömer <nico.schloemer@gmail.com>
2016-06-17 14:20:20 +0200

    use meshzoo for basic poisson example

--- examples/poisson/main.py
+++ examples/poisson/main.py
@@ -1,11 +1,16 @@
 # TODO create poisson.py by calling `form-compiler def.py poisson.py`
 import poisson
 
+import meshzoo
 import pyfvm
 from scipy.sparse import linalg
 
-# Read the mesh
-mesh, _, _ = pyfvm.reader.read('pacman.e')
+# Read the mesh using meshio
+# mesh, _, _ = pyfvm.reader.read('pacman.e')
+
+# Create mesh using meshzoo
+vertices, cells = meshzoo.rectangle.create_mesh(2.0, 1.0, 21, 11, zigzag=True)
+mesh = pyfvm.meshTri.meshTri(vertices, cells)
 
 mesh.mark_subdomains([
         poisson.Gamma0(),
