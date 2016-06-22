423ac45d2adc5ccebc302f3fed3d2be26daf43d4
Nico Schlömer <nico.schloemer@gmail.com>
2016-06-22 17:43:39 +0200

    indentation in setup.py

--- pyfvm/__init__.py
+++ pyfvm/__init__.py
@@ -14,6 +14,7 @@ __all__ = [
     'meshTetra',
     'reader'
     ]
+
 __version__ = '0.1.0'
 __author__ = 'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
--- setup.py
+++ setup.py
@@ -17,27 +17,28 @@ def read(fname):
         content = ''
     return content
 
-setup(name='pyfvm',
-      version=__version__,
-      author=__author__,
-      author_email=__author_email__,
-      packages=['pyfvm'],
-      description='Finite Volume Discretizations for Python',
-      long_description=read('README.rst'),
-      url='https://github.com/nschloe/pyfvm',
-      download_url='https://github.com/nschloe/pyfvm/releases',
-      license='License :: OSI Approved :: MIT License',
-      platforms='any',
-      requires=['numpy', 'scipy', 'sympy', 'vtk'],
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Scientific/Engineering :: Mathematics'
-          ],
-      scripts=[
-          'tools/pyfvm-compiler',
-          ],
-      )
+setup(
+    name='pyfvm',
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    packages=['pyfvm'],
+    description='Finite Volume Discretizations for Python',
+    long_description=read('README.rst'),
+    url='https://github.com/nschloe/pyfvm',
+    download_url='https://github.com/nschloe/pyfvm/releases',
+    license='License :: OSI Approved :: MIT License',
+    platforms='any',
+    requires=['numpy', 'scipy', 'sympy', 'vtk'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Mathematics'
+        ],
+    scripts=[
+        'tools/pyfvm-compiler',
+        ]
+    )
