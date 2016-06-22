f7a5a6231eb68666560d3237d6325a8aaceda51b
Nico Schlömer <nico.schloemer@gmail.com>
2016-06-22 20:37:05 +0200

    a bunch of style fixes

--- experimental/mesh2d_shell.py
+++ experimental/mesh2d_shell.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
-# ==============================================================================
 import numpy as np
 
 import mesh2d
-# ==============================================================================
-class mesh2D_shell( mesh2d ):
-    # --------------------------------------------------------------------------
+
+
+class mesh2D_shell(mesh2d):
     def __init__(self,
                  nodes,
                  cellsNodes,
-                 cellsEdges = None,
-                 edgesNodes = None,
-                 edgesCells = None
+                 cellsEdges=None,
+                 edgesNodes=None,
+                 edgesCells=None
                  ):
         # It would be sweet if we could handle cells and the rest as arrays
         # with fancy dtypes such as
@@ -27,17 +26,33 @@ class mesh2D_shell( mesh2d ):
         #
         # does not seem to work for whatever reason.
         # Hence, handle cells and friends of dictionaries of np.arrays.
-        if not isinstance(nodes,np.ndarray):
-           raise TypeError('For performace reasons, build nodes as np.empty(num_nodes, dtype=np.dtype((float, 3)))')
+        if not isinstance(nodes, np.ndarray):
+            raise TypeError(
+                    'For performace reasons, '
+                    'build nodes as '
+                    'np.empty(num_nodes, dtype=np.dtype((float, 3)))'
+                    )
 
-        if not isinstance(cellsNodes,np.ndarray):
-           raise TypeError('For performace reasons, build cellsNodes as np.empty(num_nodes, dtype=np.dtype((int, 3)))')
+        if not isinstance(cellsNodes, np.ndarray):
+            raise TypeError(
+                'For performace reasons, '
+                'build cellsNodes as '
+                'np.empty(num_nodes, dtype=np.dtype((int, 3)))'
+                )
 
-        if cellsEdges is not None and not isinstance(cellsEdges,np.ndarray):
-           raise TypeError('For performace reasons, build cellsEdges as np.empty(num_nodes, dtype=np.dtype((int, 3)))')
+        if cellsEdges is not None and not isinstance(cellsEdges, np.ndarray):
+            raise TypeError(
+                'For performace reasons, '
+                'build cellsEdges as '
+                'np.empty(num_nodes, dtype=np.dtype((int, 3)))'
+                )
 
-        if edgesNodes is not None and  not isinstance(edgesNodes,np.ndarray):
-           raise TypeError('For performace reasons, build edgesNodes as np.empty(num_nodes, dtype=np.dtype((int, 2)))')
+        if edgesNodes is not None and not isinstance(edgesNodes, np.ndarray):
+            raise TypeError(
+                'For performace reasons, '
+                'build edgesNodes as '
+                'np.empty(num_nodes, dtype=np.dtype((int, 2)))'
+                )
 
         self.nodes = nodes
         self.edgesNodes = edgesNodes
@@ -50,31 +65,33 @@ class mesh2D_shell( mesh2d ):
         self.control_volumes = None
 
         self.vtk_mesh = None
-    # --------------------------------------------------------------------------
+
     def create_cells_volume(self):
         '''Returns the area of triangle spanned by the two given edges.'''
         import vtk
         num_cells = len(self.cellsNodes)
         self.cellsVolume = np.empty(num_cells, dtype=float)
         for cell_id, cellNodes in enumerate(self.cellsNodes):
-            #edge0 = node0 - node1
-            #edge1 = node1 - node2
-            #self.cellsVolume[cell_id] = 0.5 * np.linalg.norm( np.cross( edge0, edge1 ) )
+            # edge0 = node0 - node1
+            # edge1 = node1 - node2
+            # self.cellsVolume[cell_id] = \
+            #    0.5 * np.linalg.norm( np.cross( edge0, edge1 ) )
             x = self.nodes[cellNodes]
             self.cellsVolume[cell_id] = \
-               abs(vtk.vtkTriangle.TriangleArea(x[0], x[1], x[2]))
+                abs(vtk.vtkTriangle.TriangleArea(x[0], x[1], x[2]))
         return
-    # --------------------------------------------------------------------------
-    def create_cell_circumcenters( self ):
+
+    def create_cell_circumcenters(self):
         '''Computes the center of the circumsphere of each cell.
         '''
         import vtk
         num_cells = len(self.cellsNodes)
-        self.cell_circumcenters = np.empty(num_cells, dtype=np.dtype((float,3)))
+        self.cell_circumcenters = \
+            np.empty(num_cells, dtype=np.dtype((float, 3)))
         for cell_id, cellNodes in enumerate(self.cellsNodes):
             x = self.nodes[cellNodes]
             # Project triangle to 2D.
-            v = np.empty(3, dtype=np.dtype((float,2)))
+            v = np.empty(3, dtype=np.dtype((float, 2)))
             vtk.vtkTriangle.ProjectTo2D(x[0], x[1], x[2],
                                         v[0], v[1], v[2])
             # Get the circumcenter in 2D.
@@ -87,40 +104,40 @@ class mesh2D_shell( mesh2d ):
             self.cell_circumcenters[cell_id] = \
                 bcoords[0] * x[0] + bcoords[1] * x[1] + bcoords[2] * x[2]
 
-            #a = x[0] - x[1]
-            #b = x[1] - x[2]
-            #c = x[2] - x[0]
-            #w = np.cross(a, b)
-            #omega = 2.0 * np.dot(w, w)
-            #if abs(omega) < 1.0e-10:
-                #raise ZeroDivisionError( 'The nodes don''t seem to form '
-                                        #+ 'a proper triangle.' )
-            #alpha = -np.dot(b, b) * np.dot(a, c) / omega
-            #beta  = -np.dot(c, c) * np.dot(b, a) / omega
-            #gamma = -np.dot(a, a) * np.dot(c, b) / omega
-            #m = alpha * x[0] + beta * x[1] + gamma * x[2]
+            # a = x[0] - x[1]
+            # b = x[1] - x[2]
+            # c = x[2] - x[0]
+            # w = np.cross(a, b)
+            # omega = 2.0 * np.dot(w, w)
+            # if abs(omega) < 1.0e-10:
+            #     raise ZeroDivisionError('The nodes don''t seem to form '
+            #                             'a proper triangle.')
+            # alpha = -np.dot(b, b) * np.dot(a, c) / omega
+            # beta  = -np.dot(c, c) * np.dot(b, a) / omega
+            # gamma = -np.dot(a, a) * np.dot(c, b) / omega
+            # m = alpha * x[0] + beta * x[1] + gamma * x[2]
 
-            ## Alternative implementation from
-            ## https://www.ics.uci.edu/~eppstein/junkyard/circumcenter.html
-            #a = x[1] - x[0]
-            #b = x[2] - x[0]
-            #alpha = np.dot(a, a)
-            #beta = np.dot(b, b)
-            #w = np.cross(a, b)
-            #omega = 2.0 * np.dot(w, w)
-            #m = np.empty(3)
-            #m[0] = x[0][0] + ((alpha * b[1] - beta * a[1]) * w[2]
-                              #-(alpha * b[2] - beta * a[2]) * w[1]) / omega
-            #m[1] = x[0][1] + ((alpha * b[2] - beta * a[2]) * w[0]
-                              #-(alpha * b[0] - beta * a[0]) * w[2]) / omega
-            #m[2] = x[0][2] + ((alpha * b[0] - beta * a[0]) * w[1]
-                              #-(alpha * b[1] - beta * a[1]) * w[0]) / omega
+            # # Alternative implementation from
+            # # https://www.ics.uci.edu/~eppstein/junkyard/circumcenter.html
+            # a = x[1] - x[0]
+            # b = x[2] - x[0]
+            # alpha = np.dot(a, a)
+            # beta = np.dot(b, b)
+            # w = np.cross(a, b)
+            # omega = 2.0 * np.dot(w, w)
+            # m = np.empty(3)
+            # m[0] = x[0][0] + ((alpha * b[1] - beta * a[1]) * w[2]
+            #                   -(alpha * b[2] - beta * a[2]) * w[1]) / omega
+            # m[1] = x[0][1] + ((alpha * b[2] - beta * a[2]) * w[0]
+            #                 -(alpha * b[0] - beta * a[0]) * w[2]) / omega
+            # m[2] = x[0][2] + ((alpha * b[0] - beta * a[0]) * w[1]
+            #                 -(alpha * b[1] - beta * a[1]) * w[0]) / omega
 
         return
-    # --------------------------------------------------------------------------
-    def compute_control_volumes( self ):
+
+    def compute_control_volumes(self):
         num_nodes = len(self.nodes)
-        self.control_volumes = np.zeros((num_nodes,1), dtype = float)
+        self.control_volumes = np.zeros((num_nodes, 1), dtype=float)
 
         # compute cell circumcenters
         if self.cell_circumcenters is None:
@@ -145,15 +162,17 @@ class mesh2D_shell( mesh2d ):
             # Get the circumcenters of the adjacent cells.
             cc = circumcenters[self.edgesCells[k]]
             node_ids = self.edgesNodes[k]
-            if len(cc) == 2: # interior cell
+            if len(cc) == 2:  # interior cell
                 # TODO check out if this holds true for bent surfaces too
                 coedge = cc[1] - cc[0]
-            elif len(cc) == 1: # boundary cell
+            elif len(cc) == 1:  # boundary cell
                 node_coords = self.nodes[node_ids]
                 edge_midpoint = 0.5 * (node_coords[0] + node_coords[1])
                 coedge = edge_midpoint - cc[0]
             else:
-                raise RuntimeError('An edge should have either 1 or two adjacent cells.')
+                raise RuntimeError(
+                    'An edge should have either 1 or two adjacent cells.'
+                    )
 
             # Project the coedge onto the outer normal. The two vectors should
             # be parallel, it's just the sign of the coedge length that is to
@@ -163,13 +182,13 @@ class mesh2D_shell( mesh2d ):
             self.control_volumes[node_ids] += pyramid_volume
 
         return
-    # --------------------------------------------------------------------------
+
     def _compute_edge_normals(self, edge_lengths):
         # Precompute edge normals. Do that in such a way that the
         # face normals points in the direction of the cell with the higher
         # cell ID.
         num_edges = len(self.edgesNodes)
-        edge_normals = np.empty(num_edges, dtype=np.dtype((float,3)))
+        edge_normals = np.empty(num_edges, dtype=np.dtype((float, 3)))
         for cell_id, cellEdges in enumerate(self.cellsEdges):
             # Loop over the local faces.
             for k in xrange(3):
@@ -187,16 +206,20 @@ class mesh2D_shell( mesh2d ):
                     # As reference, any point in face can be taken, e.g.,
                     # the first face corner point
                     # self.edgesNodes[edge_id][0].
-                    edge_normals[edge_id] = edge_nodes[0] \
-                                      - self.nodes[other_node_id]
+                    edge_normals[edge_id] = \
+                        edge_nodes[0] - self.nodes[other_node_id]
                     # Make it orthogonal to the face.
-                    edge_dir = (edge_nodes[1] - edge_nodes[0]) / edge_lengths[edge_id]
-                    edge_normals[edge_id] -= np.dot(edge_normals[edge_id], edge_dir) * edge_dir
+                    edge_dir = (edge_nodes[1] - edge_nodes[0]) / \
+                        edge_lengths[edge_id]
+                    edge_normals[edge_id] -= np.dot(
+                            edge_normals[edge_id], edge_dir
+                            ) * edge_dir
                     # Normalization.
-                    edge_normals[edge_id] /= np.linalg.norm(edge_normals[edge_id])
+                    edge_normals[edge_id] /= \
+                        np.linalg.norm(edge_normals[edge_id])
         return edge_normals
-    # --------------------------------------------------------------------------
-    def show(self, highlight_nodes = None):
+
+    def show(self, highlight_nodes=None):
         '''Plot the mesh.'''
         if highlight_nodes is None:
             highlight_nodes = []
@@ -205,13 +228,13 @@ class mesh2D_shell( mesh2d ):
             raise RuntimeError('Can only show mesh when edges are created.')
 
         import matplotlib.pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
+        # from mpl_toolkits.mplot3d import Axes3D
 
         if self.cell_circumcenters is None:
             self.create_cell_circumcenters()
 
         fig = plt.figure()
-        #ax = fig.gca(projection='3d')
+        # ax = fig.gca(projection='3d')
         ax = fig.gca()
         plt.axis('equal')
 
@@ -219,9 +242,9 @@ class mesh2D_shell( mesh2d ):
         col = 'k'
         for node_ids in self.edgesNodes:
             x = self.nodes[node_ids]
-            ax.plot(x[:,0],
-                    x[:,1],
-                    #x[:,2],
+            ax.plot(x[:, 0],
+                    x[:, 1],
+                    # x[:,2],
                     col)
 
         # Highlight nodes and their covolumes.
@@ -240,21 +263,24 @@ class mesh2D_shell( mesh2d ):
                     adjacent_cells = self.edgesCells[edge_id]
                     ccs = self.cell_circumcenters[adjacent_cells]
                     if len(ccs) == 2:
-                        ax.plot( [ccs[0][0], ccs[1][0]],
-                                 [ccs[0][1], ccs[1][1]],
-                                 [ccs[0][2], ccs[1][2]],
-                                 color = covolume_col )
+                        ax.plot([ccs[0][0], ccs[1][0]],
+                                [ccs[0][1], ccs[1][1]],
+                                [ccs[0][2], ccs[1][2]],
+                                color=covolume_col
+                                )
                     elif len(ccs) == 1:
-                        edge_midpoint = 0.5 * (self.nodes[node_ids[0]]
-                                              +self.nodes[node_ids[1]])
-                        ax.plot( [ccs[0][0], edge_midpoint[0]],
-                                 [ccs[0][1], edge_midpoint[1]],
-                                 [ccs[0][2], edge_midpoint[2]],
-                                 color = covolume_col )
+                        edge_midpoint = 0.5 * (
+                            self.nodes[node_ids[0]] + self.nodes[node_ids[1]]
+                            )
+                        ax.plot([ccs[0][0], edge_midpoint[0]],
+                                [ccs[0][1], edge_midpoint[1]],
+                                [ccs[0][2], edge_midpoint[2]],
+                                color=covolume_col
+                                )
                     else:
-                        raise RuntimeError('An edge has to have either 1 or 2 adjacent cells.')
+                        raise RuntimeError(
+                            'An edge has to have either 1 or 2 adjacent cells.'
+                            )
 
         plt.show()
         return
-    # --------------------------------------------------------------------------
-# ==============================================================================
