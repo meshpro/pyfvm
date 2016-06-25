d0b7fd968643f5f7780e94d5ca62e11bf383fc5f
Nico Schlömer <nico.schloemer@gmail.com>
2016-06-25 23:03:22 +0200

    add test for singular perturbation problem

--- tests/fvm.py
+++ tests/fvm.py
@@ -44,8 +44,46 @@ class TestPDEs(unittest.TestCase):
                 k0 = k
                 break
 
-        # self.assertNotEqual(k0, -1)
+        self.assertNotEqual(k0, -1)
         self.assertAlmostEqual(x[k0], 0.0735267092334, delta=1.0e-7)
+
+        return
+
+
+    def test_singular_perturbation(self):
+        import meshzoo
+        from scipy.sparse import linalg
+
+        # Define the problem
+        class Poisson(LinearFvmProblem):
+            @staticmethod
+            def apply(u):
+                return integrate(lambda x: - 1.0e-2 * n_dot_grad(u(x)), dS) \
+                       + integrate(lambda x: u(x), dV) \
+                       - integrate(lambda x: 1.0, dV)
+            dirichlet = [(lambda x: 0.0, ['Boundary'])]
+
+        # Create mesh using meshzoo
+        vertices, cells = meshzoo.rectangle.create_mesh(
+                0.0, 1.0, 0.0, 1.0,
+                21, 21,
+                zigzag=True
+                )
+        mesh = pyfvm.meshTri.meshTri(vertices, cells)
+
+        linear_system = pyfvm.discretize(Poisson, mesh)
+
+        x = linalg.spsolve(linear_system.matrix, linear_system.rhs)
+
+        k0 = -1
+        for k, coord in enumerate(mesh.node_coords):
+            # print(coord - [0.5, 0.5, 0.0])
+            if numpy.linalg.norm(coord - [0.5, 0.5, 0.0]) < 1.0e-5:
+                k0 = k
+                break
+
+        self.assertNotEqual(k0, -1)
+        self.assertAlmostEqual(x[k0], 0.97335485230869123, delta=1.0e-7)
 
         return
 
