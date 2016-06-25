223e558c99528aa8de10a5b49390e7435206ff80
Nico Schlömer <nico.schloemer@gmail.com>
2016-06-25 21:24:29 +0200

    add example for neumann boundary conditions

---
+++ examples/neumann-boundary/main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import pyfvm
+from pyfvm.compiler.form_language import *
+import meshzoo
+from scipy.sparse import linalg
+from sympy import sin
+from numpy import pi
+
+
+class D1(Subdomain):
+    def is_inside(self, x): return x[1] < 0.5
+    is_boundary_only = True
+
+
+class Poisson(LinearFvmProblem):
+    @staticmethod
+    def apply(u):
+        return integrate(lambda x: -n_dot_grad(u(x)), dS) \
+                + integrate(lambda x: 3.0, dGamma) \
+                - integrate(lambda x: 1.0, dV)
+
+    dirichlet = [(lambda x: 0.0, ['D1'])]
+
+vertices, cells = meshzoo.rectangle.create_mesh(
+        0.0, 1.0,
+        0.0, 1.0,
+        51, 51,
+        zigzag=True
+        )
+mesh = pyfvm.meshTri.meshTri(vertices, cells)
+
+mesh.mark_subdomains([D1()])
+
+linear_system = pyfvm.discretize(Poisson, mesh)
+
+x = linalg.spsolve(linear_system.matrix, linear_system.rhs)
+
+mesh.write('out.vtu', point_data={'x': x})
