edbb2732d02d4e43df89436c62b7bd859d8b17c4
Nico Schlömer <nico.schloemer@gmail.com>
2016-06-27 17:48:21 +0200

    add test for neumann boundary

--- tests/fvm_test.py
+++ tests/fvm_test.py
@@ -134,6 +134,48 @@ class TestPDEs(unittest.TestCase):
 
         return
 
+    def test_neumann(self):
+        import meshzoo
+        from scipy.sparse import linalg
+
+        class D1(Subdomain):
+            def is_inside(self, x): return x[1] < 0.5
+            is_boundary_only = True
+
+        # Define the problem
+        class Poisson(LinearFvmProblem):
+            @staticmethod
+            def apply(u):
+                return integrate(lambda x: - n_dot_grad(u(x)), dS) \
+                       + integrate(lambda x: 3.0, dGamma) \
+                       - integrate(lambda x: 1.0, dV)
+            dirichlet = [(lambda x: 0.0, ['D1'])]
+
+        # Create mesh using meshzoo
+        vertices, cells = meshzoo.rectangle.create_mesh(
+                0.0, 1.0, 0.0, 1.0,
+                21, 21,
+                zigzag=True
+                )
+        mesh = pyfvm.meshTri.meshTri(vertices, cells)
+        mesh.mark_subdomains([D1()])
+
+        linear_system = pyfvm.discretize(Poisson, mesh)
+
+        x = linalg.spsolve(linear_system.matrix, linear_system.rhs)
+
+        k0 = -1
+        for k, coord in enumerate(mesh.node_coords):
+            # print(coord - [0.5, 0.5, 0.0])
+            if numpy.linalg.norm(coord - [0.5, 0.5, 0.0]) < 1.0e-5:
+                k0 = k
+                break
+
+        self.assertNotEqual(k0, -1)
+        self.assertAlmostEqual(x[k0], -1.3249459366260112, delta=1.0e-7)
+
+        return
+
 
 if __name__ == '__main__':
     unittest.main()
