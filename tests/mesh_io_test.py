ffbd84201c1bea748c3c91c05ebe7775d9a76de3
Nico Schlömer <nico.schloemer@gmail.com>
2016-06-28 14:37:35 +0200

    3d writing test

--- tests/mesh_io_test.py
+++ tests/mesh_io_test.py
@@ -11,13 +11,30 @@ class TestLulz(unittest.TestCase):
     def setUp(self):
         return
 
-    def test_io(self):
+    def test_io_2d(self):
         vertices, cells = meshzoo.rectangle.create_mesh(
                 0.0, 1.0, 0.0, 1.0,
                 2, 2,
                 zigzag=True
                 )
         mesh = pyfvm.meshTri.meshTri(vertices, cells)
+        mesh.write('test.vtu')
+
+        mesh2, _, _ = pyfvm.reader.read('test.vtu')
+
+        for k in range(len(mesh.cells['nodes'])):
+            self.assertEqual(
+                    tuple(mesh.cells['nodes'][k]),
+                    tuple(mesh2.cells['nodes'][k])
+                    )
+        return
+
+    def test_io_3d(self):
+        vertices, cells = meshzoo.cube.create_mesh(
+                0.0, 1.0, 0.0, 1.0, 0.0, 1.0,
+                2, 2, 2
+                )
+        mesh = pyfvm.meshTetra.meshTetra(vertices, cells)
         mesh.write('test.vtu')
 
         mesh2, _, _ = pyfvm.reader.read('test.vtu')
