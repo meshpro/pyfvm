443139f41f0b214be04b6b719c8c807e64ba4bae
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-08 21:02:31 +0200

    better mesh tests

--- tests/cv_tests.py
+++ tests/cv_tests.py
@@ -12,25 +12,30 @@ class TestControlVolumes(unittest.TestCase):
     def setUp(self):
         return
 
-    def _run_test(self, mesh, actual_values):
+    def _run_test(self, mesh, volume, cv_norms):
         # Compute the control volumes.
         if mesh.control_volumes is None:
             mesh.compute_control_volumes()
         tol = 1.0e-5
+
         # Check the volume by summing over the cell volume.
         if mesh.cell_volumes is None:
             mesh.create_cell_volumes()
         vol2 = sum(mesh.cell_volumes)
-        self.assertAlmostEqual(actual_values[0], vol2, delta=tol)
+
+        self.assertAlmostEqual(volume, vol2, delta=tol)
+
         # Check the volume by summing over the absolute value of the
         # control volumes.
         vol = numpy.linalg.norm(mesh.control_volumes, ord=1)
-        self.assertAlmostEqual(actual_values[0], vol, delta=tol)
+        self.assertAlmostEqual(volume, vol, delta=tol)
+
         # Check control volume norms.
         norm = numpy.linalg.norm(mesh.control_volumes, ord=2)
-        self.assertAlmostEqual(actual_values[1], norm, delta=tol)
+        self.assertAlmostEqual(cv_norms[0], norm, delta=tol)
         norm = numpy.linalg.norm(mesh.control_volumes, ord=numpy.Inf)
-        self.assertAlmostEqual(actual_values[2], norm, delta=tol)
+        self.assertAlmostEqual(cv_norms[1], norm, delta=tol)
+
         return
 
     # def test_degenerate_small(self):
@@ -57,10 +62,7 @@ class TestControlVolumes(unittest.TestCase):
         filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 'rectanglesmall.e')
         mesh, _, _ = pyfvm.reader.read(filename)
-        actual_values = [10.0,
-                         5.0,
-                         2.5]
-        self._run_test(mesh, actual_values)
+        self._run_test(mesh, 10, [5.0, 2.5])
         return
 
     def test_arrow3d(self):
@@ -80,11 +82,11 @@ class TestControlVolumes(unittest.TestCase):
         # if mesh.edgesNodes is None:
         #     mesh.create_adjacent_entities()
         # mesh.show_edge(5)
-        actual_values = [1.2,
-                         0.58276428453480855,
-                         0.459
-                         ]
-        self._run_test(mesh, actual_values)
+        self._run_test(
+                mesh,
+                1.2,
+                [0.58276428453480855, 0.459]
+                )
         return
 
     def test_tetrahedron(self):
@@ -92,45 +94,44 @@ class TestControlVolumes(unittest.TestCase):
                                 'tetrahedron.e')
         mesh, _, _ = pyfvm.reader.read(filename)
         # mesh.show_edge(54)
-        actual_values = [
+        self._run_test(
+                mesh,
                 64.150028545707983,
-                15.243602636687179,
-                7.7180603065060023
-                ]
-        self._run_test(mesh, actual_values)
+                [15.243602636687179, 7.7180603065060023]
+                )
         return
 
     def test_pacman(self):
         filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 'pacman.e')
         mesh, _, _ = pyfvm.reader.read(filename)
-        actual_values = [302.52270072101,
-                         15.3857579093391,
-                         1.12779746704366
-                         ]
-        self._run_test(mesh, actual_values)
+        self._run_test(
+                mesh,
+                302.52270072101,
+                [15.3857579093391, 1.12779746704366]
+                )
         return
 
     def test_shell(self):
         filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 'shell.e')
         mesh, _, _ = pyfvm.reader.read(filename)
-        actual_values = [3.46410161513775,
-                         1.63299316185545,
-                         1.15470053837925
-                         ]
-        self._run_test(mesh, actual_values)
+        self._run_test(
+                mesh,
+                3.46410161513775,
+                [1.63299316185545, 1.15470053837925]
+                )
         return
 
     def test_sphere(self):
         filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 'sphere.e')
         mesh, _, _ = pyfvm.reader.read(filename)
-        actual_values = [11.9741927059035,
-                         1.39047542328083,
-                         0.198927169088121
-                         ]
-        self._run_test(mesh, actual_values)
+        self._run_test(
+                mesh,
+                11.9741927059035,
+                [1.39047542328083, 0.198927169088121]
+                )
         return
 
     def test_cubesmall(self):
@@ -141,19 +142,22 @@ class TestControlVolumes(unittest.TestCase):
                          3.53553390593274,
                          1.25
                          ]
-        self._run_test(mesh, actual_values)
+        self._run_test(
+                mesh,
+                10.0,
+                [3.53553390593274, 1.25]
+                )
         return
 
     def test_brick(self):
         filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 'brick-w-hole.e')
         mesh, _, _ = pyfvm.reader.read(filename)
-        actual_values = [
+        self._run_test(
+                mesh,
                 388.68629169464117,
-                16.661401941985677,
-                1.4684734547497671
-                ]
-        self._run_test(mesh, actual_values)
+                [16.661401941985677, 1.4684734547497671]
+                )
         return
 
 if __name__ == '__main__':
