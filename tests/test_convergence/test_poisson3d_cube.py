71071e541a3e87a49730c8d75b9adf1d2df89a88
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-19 00:50:22 +0200

    add convergence test for poisson on a cube

---
+++ tests/test_convergence/test_poisson3d_cube.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import helpers
+import pyfvm
+from pyfvm.form_language import *
+import meshzoo
+from sympy import sin
+import numpy
+from numpy import pi
+import unittest
+
+
+class Poisson(LinearFvmProblem):
+    @staticmethod
+    def apply(u):
+        return integrate(lambda x: -n_dot_grad(u(x)), dS) - \
+            integrate(
+              lambda x: 3*pi**2 * sin(pi*x[0]) * sin(pi*x[1]) * sin(pi*x[2]),
+              dV
+              )
+
+    dirichlet = [
+            (lambda x: 0.0, ['Boundary'])
+            ]
+
+
+def exact_sol(x):
+    return numpy.sin(pi*x[0]) * numpy.sin(pi*x[1]) * numpy.sin(pi*x[2])
+
+
+def get_mesh(k):
+    n = 2**(k+1)
+    vertices, cells = meshzoo.cube.create_mesh(
+            0.0, 1.0,
+            0.0, 1.0,
+            0.0, 1.0,
+            n+1, n+1, n+1
+            )
+    return pyfvm.meshTetra.meshTetra(vertices, cells, mode='algebraic')
+
+
+class ConvergencePoisson3dCubeTest(unittest.TestCase):
+
+    def setUp(self):
+        return
+
+    @staticmethod
+    def solve(do_print=False):
+        return helpers.perform_convergence_tests(
+            Poisson,
+            exact_sol,
+            get_mesh,
+            range(4),
+            do_print=do_print
+            )
+
+    def test(self):
+        H, error_norm_1, error_norm_inf, order_1, order_inf = self.solve()
+
+        expected_order = 2
+        tol = 1.0e-2
+        self.assertGreater(order_1[-1], expected_order - tol)
+        self.assertGreater(order_inf[-1], expected_order - tol)
+
+        return
+
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+
+    H, error_norm_1, error_norm_inf, order_1, order_inf = \
+        ConvergencePoisson3dCubeTest.solve(do_print=True)
+
+    helpers.plot_error_data(H, error_norm_1, error_norm_inf)
+    plt.show()
