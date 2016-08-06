6dbd7e08e5a74e1fe2ea3f2c7b36856f6e9a7904
Nico Schlömer <nico.schloemer@gmail.com>
2016-08-06 16:27:31 +0200

    move split() into discretize_linear

--- pyfvm/discretize_linear.py
+++ pyfvm/discretize_linear.py
@@ -1,12 +1,46 @@
 # -*- coding: utf-8 -*-
 #
 import numpy
-from .helpers import split_affine_linear_nonlinear as split
 from . import form_language
 from .form_language import n
 import linear_fvm_problem
 import sympy
 from sympy.matrices.expressions.matexpr import MatrixExpr, MatrixSymbol
+
+
+def split(expr, variables):
+    '''Split affine, linear, and nonlinear part of of expr w.r.t. variables.
+    '''
+    if isinstance(expr, float):
+        return expr, 0, 0
+
+    input_is_list = True
+    if not isinstance(variables, list):
+        input_is_list = False
+        variables = [variables]
+
+    # See <https://github.com/sympy/sympy/issues/11475> on why we need expand()
+    # here.
+    expr = expr.expand()
+    affine = expr
+    print('affine ', affine)
+    linear = []
+    for var in variables:
+        linear.append(sympy.diff(expr, var).coeff(var, 0))
+        affine = affine.coeff(var, 0)
+        print('affine ', affine)
+
+    # The rest is nonlinear
+    nonlinear = expr - affine
+    for var, coeff in zip(variables, linear):
+        nonlinear -= var * coeff
+    nonlinear = sympy.simplify(nonlinear)
+
+    if not input_is_list:
+        assert len(linear) == 1
+        linear = linear[0]
+
+    return affine, linear, nonlinear
 
 
 class EdgeKernel(object):
