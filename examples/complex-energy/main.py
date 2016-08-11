4069cde106204ed9c1ad20670b8034fca79f31e5
Nico Schlömer <nico.schloemer@gmail.com>
2016-08-11 17:54:03 +0200

    simplify complex energy example

--- examples/complex-energy/main.py
+++ examples/complex-energy/main.py
@@ -12,15 +12,8 @@ class EnergyEdgeKernel(object):
         return
 
     def eval(self, mesh, edge_ids):
-        X = mesh.node_coords[mesh.edges['nodes'][edge_ids]]
-        x0 = X[:, 0, :].T
-        x1 = X[:, 1, :].T
-        edge_midpoint = 0.5 * (x0 + x1)
-        edge = x1 - x0
         edge_ce_ratio = mesh.ce_ratios[edge_ids]
-
         beta = 1.0
-
         return numpy.array([
             [edge_ce_ratio, -edge_ce_ratio * numpy.exp(1j * beta)],
             [-edge_ce_ratio * numpy.exp(-1j * beta), edge_ce_ratio]
@@ -32,10 +25,9 @@ mesh = pyfvm.meshTri.meshTri(vertices, cells)
 matrix = pyfvm.get_fvm_matrix(mesh, [EnergyEdgeKernel()], [], [], [])
 rhs = mesh.control_volumes.copy()
 
-# Smoothed aggregation.
 sa = pyamg.smoothed_aggregation_solver(matrix, smooth='energy')
 u = sa.solve(rhs, tol=1e-10)
 
 # Cannot write complex data ot VTU; split real and imaginary parts first.
-u2 = numpy.ascontiguousarray(numpy.vstack((u.real, u.imag)).T)
+u2 = numpy.dstack((u.real, u.imag))[0]
 mesh.write('out.vtu', point_data={'u': u2})
