3840a6feb19b1323439b7e64bcd7cb884dbfe21e
Nico Schlömer <nico.schloemer@gmail.com>
2016-08-12 22:24:16 +0200

    first working ginzburg-landau

--- pyfvm/nonlinear_methods.py
+++ pyfvm/nonlinear_methods.py
@@ -13,16 +13,7 @@ def newton(f, jacobian_solver, u0, tol=1.0e-10, max_iter=20, verbose=True):
 
     k = 0
     while nrm > tol:
-        # jac = jacobian(u)
-        # du = linalg.spsolve(jac, -fu)
-
         du = jacobian_solver(u, -fu)
-
-        # # create linear system and solve
-        # linear_system = krypy.LinearSystem(A=jac, b=-fu)
-        # sol = krypy.Gmres(linear_system)
-        # print(sol)
-
         u += du
         fu = f(u)
         nrm = numpy.linalg.norm(fu)
