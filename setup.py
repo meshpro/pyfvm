86eb95b56cf35772f6548b983b714b5afd99ec79
Nico Schlömer <nico.schloemer@gmail.com>
2017-02-06 13:11:08 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 #
-#from distutils.core import setup
 from setuptools import setup, find_packages
 import os
 import codecs
 
-from pyfvm import __version__, __author__, __author_email__
+# https://packaging.python.org/single_source_version/
+base_dir = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(base_dir, 'pyfvm', '__about__.py')) as f:
+    exec(f.read(), about)
 
 
 def read(fname):
@@ -18,11 +21,12 @@ def read(fname):
         content = ''
     return content
 
+
 setup(
     name='PyFVM',
-    version=__version__,
-    author=__author__,
-    author_email=__author_email__,
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
     packages=find_packages(),
     description='Finite Volume Discretizations for Python',
     long_description=read('README.rst'),
@@ -39,6 +43,7 @@ setup(
         'pyamg',
         'scipy',
         'sympy',
+        'voropy',
         ],
     classifiers=[
         'Development Status :: 4 - Beta',
