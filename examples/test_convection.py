6833417f9771bc49124534b48e890711b2fcbff1
Nico Schlömer <nico.schloemer@gmail.com>
2017-05-23 23:12:34 +0200

    make two examples tests

---
+++ examples/test_convection.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import pyfvm
+from pyfvm.form_language import integrate, n_dot_grad, n_dot, dS, dV, Boundary
+import meshzoo
+from scipy.sparse import linalg
+import sympy
+import voropy
+
+
+def test():
+    class DC(object):
+        def apply(self, u):
+            a = sympy.Matrix([2, 1, 0])
+            return (
+                integrate(lambda x: -n_dot_grad(u(x)) + n_dot(a) * u(x), dS)
+                - integrate(lambda x: 1.0, dV)
+                )
+
+        def dirichlet(self, u):
+            return [(u, Boundary())]
+
+    vertices, cells = meshzoo.rectangle(0.0, 1.0, 0.0, 1.0, 51, 51)
+    mesh = voropy.mesh_tri.MeshTri(vertices, cells)
+
+    matrix, rhs = pyfvm.discretize_linear(DC(), mesh)
+
+    u = linalg.spsolve(matrix, rhs)
+
+    mesh.write('out.vtu', point_data={'u': u})
+    return
+
+
+if __name__ == '__main__':
+    test()
