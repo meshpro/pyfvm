b661ade33201a32ccdd44c99e71561417b6b5f98
Nico Schlömer <nico.schloemer@gmail.com>
2017-05-23 23:22:46 +0200

    test more examples

---
+++ examples/test_singular_perturbation.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+import meshzoo
+import pyfvm
+from pyfvm.form_language import integrate, n_dot_grad, dS, dV, Boundary
+from scipy.sparse import linalg
+import voropy
+
+
+def test():
+    class Singular(object):
+        def apply(self, u):
+            return integrate(lambda x: - 1.0e-2 * n_dot_grad(u(x)), dS) \
+                   + integrate(u, dV) \
+                   - integrate(lambda x: 1.0, dV)
+
+        def dirichlet(self, u):
+            return [(u, Boundary())]
+
+    vertices, cells = meshzoo.rectangle(0.0, 1.0, 0.0, 1.0, 51, 51)
+    mesh = voropy.mesh_tri.MeshTri(vertices, cells)
+
+    matrix, rhs = pyfvm.discretize_linear(Singular(), mesh)
+
+    u = linalg.spsolve(matrix, rhs)
+
+    mesh.write('out.vtu', point_data={'u': u})
+    return
+
+
+if __name__ == '__main__':
+    test()
