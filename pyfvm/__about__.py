6ca46e816169390ff46e8ff6bad1cc84c119fa4f
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-25 20:05:52 +0200

    __about__ fix

--- pyfvm/__about__.py
+++ pyfvm/__about__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 __version__ = '0.2.3'
-__author__ = 'Nico Schlömer'
+__author__ = u'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
 __license__ = 'License :: OSI Approved :: MIT License'
 __status__ = 'Development Status :: 4 - Beta'
