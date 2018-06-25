5a5cde83b178040f108850680633021e86c5dc64
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-25 20:11:21 +0200

    another metadata fix

--- pyfvm/__about__.py
+++ pyfvm/__about__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
 __author__ = u"Nico Schlömer"
-__email__ = "nico.schloemer@gmail.com"
-__copyright__ = u"Copyright (c) 2012-2018, {} <{}>".format(__author__, __email__)
+__author_email__ = "nico.schloemer@gmail.com"
+__copyright__ = u"Copyright (c) 2012-2018, {} <{}>".format(__author__, __author_email__)
 __license__ = "License :: OSI Approved :: MIT License"
 __version__ = "0.2.3"
 __status__ = "Development Status :: 4 - Beta"
