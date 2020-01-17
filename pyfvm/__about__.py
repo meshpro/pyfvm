d246803bea90f47800918fc32f35a462dc22c5af
Nico Schlömer <nico.schloemer@gmail.com>
2020-01-17 14:55:22 +0100

    bump year, add license file

--- pyfvm/__about__.py
+++ pyfvm/__about__.py
@@ -1,6 +1,6 @@
 __author__ = "Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
-__copyright__ = f"Copyright (c) 2012-2018, {__author__} <{__author_email__}>"
+__copyright__ = f"Copyright (c) 2012-2020, {__author__} <{__author_email__}>"
 __license__ = "License :: OSI Approved :: MIT License"
 __version__ = "0.3.0"
 __status__ = "Development Status :: 4 - Beta"
