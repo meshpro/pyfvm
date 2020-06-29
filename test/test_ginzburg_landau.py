542008511ae309ba02e31f271997edd8dfaa2fa7
Nico Schlömer <nico.schloemer@gmail.com>
2020-06-29 23:36:38 +0200

    add some test files as lfs

--- test/test_ginzburg_landau.py
+++ test/test_ginzburg_landau.py
@@ -1,35 +1,17 @@
 """
 These tests are against the reference values from pynosh.
 """
-import os
-import shutil
+import pathlib
 
 import numpy
 import pytest
-import requests
 
 import meshio
 import meshplex
 import pyfvm
 import pykry
 
-
-def download_mesh(name):
-    filename = os.path.join("/tmp", name)
-    if not os.path.exists(filename):
-        print(f"Downloading {name}...")
-        url = "https://github.com/nschloe/pynosh/raw/master/test/"
-        r = requests.get(url + name, stream=True)
-        if not r.ok:
-            raise RuntimeError(
-                f"Download error ({r.url}, return code {r.status_code})."
-            )
-        # save the mesh in /tmp
-        with open(filename, "wb") as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
-
-    return filename
+this_dir = pathlib.Path(__file__).resolve().parent
 
 
 class Energy:
@@ -39,8 +21,6 @@ class Energy:
     def __init__(self, mu):
         self.magnetic_field = mu * numpy.array([0.0, 0.0, 1.0])
         self.subdomains = [None]
-
-        return
 
     def eval(self, mesh, cell_mask):
         nec = mesh.idx_hierarchy[..., cell_mask]
@@ -96,22 +76,21 @@ class Energy:
 @pytest.mark.parametrize(
     "filename, control_values",
     [
-        ("rectanglesmall.e", [0.0063121712308067401, 10.224658806561596]),
-        ("pacman.e", [0.37044264296585938, 10.000520856079092]),
+        ("rectanglesmall.vtu", [0.0063121712308067401, 10.224658806561596]),
+        ("pacman.vtu", [0.37044264296585938, 10.000520856079092]),
         # geometric ce_ratios
-        ("cubesmall.e", [0.00012499993489764605, 10.062484361987309]),
-        ("brick-w-hole.e", [0.167357712543159, 12.05581968511059]),
+        ("cubesmall.vtu", [0.00012499993489764605, 10.062484361987309]),
+        ("brick-w-hole.vtu", [0.167357712543159, 12.05581968511059]),
         # # Algebraic ce_ratios:
-        # ("cubesmall.e", [8.3541623155714007e-05, 10.058364522531498]),
-        # ("brick-w-hole.e", [0.16763276012920181, 15.131119904340618]),
+        # ("cubesmall.vtu", [8.3541623155714007e-05, 10.058364522531498]),
+        # ("brick-w-hole.vtu", [0.16763276012920181, 15.131119904340618]),
     ],
 )
 def test_keo(filename, control_values):
-    filename = download_mesh(filename)
     mu = 1.0e-2
 
     # read the mesh
-    mesh = meshplex.read(filename)
+    mesh = meshplex.read(this_dir / "meshes" / filename)
 
     keo = pyfvm.get_fvm_matrix(mesh, edge_kernels=[Energy(mu)])
 
@@ -131,43 +110,45 @@ def test_keo(filename, control_values):
     #   Im(K)  Re(K).
     K = abs(keo.real) + abs(keo.imag)
     assert abs(control_values[1] - numpy.max(K.sum(0))) < tol
-    return
 
 
 @pytest.mark.parametrize(
     "filename, control_values",
     [
-        ("rectanglesmall.e", [20.0126243424616, 20.0063121712308, 0.00631217123080606]),
-        ("pacman.e", [605.78628672795264, 605.41584408498682, 0.37044264296586299]),
+        (
+            "rectanglesmall.vtu",
+            [20.0126243424616, 20.0063121712308, 0.00631217123080606],
+        ),
+        ("pacman.vtu", [605.78628672795264, 605.41584408498682, 0.37044264296586299]),
         # Geometric ce_ratios:
         (
-            "cubesmall.e",
+            "cubesmall.vtu",
             [20.000249999869794, 20.000124999934897, 0.00012499993489734074],
         ),
-        ("brick-w-hole.e", [777.7072988143686, 777.5399411018254, 0.16735771254316]),
+        ("brick-w-hole.vtu", [777.7072988143686, 777.5399411018254, 0.16735771254316]),
         (
-            "tetrahedron.e",
+            "tetrahedron.vtu",
             [128.3145663425826, 128.3073117169993, 0.0072546255832996644],
         ),
-        ("tet.e", [128.316760714389, 128.30840983471703, 0.008350879671951375]),
+        ("tet.vtu", [128.316760714389, 128.30840983471703, 0.008350879671951375]),
         # Algebraic ce_ratios:
         # (
-        #     "cubesmall.e",
+        #     "cubesmall.vtu",
         #     [20.000167083246311, 20.000083541623155, 8.3541623155658495e-05],
         # ),
         # (
-        #     "brick-w-hole.e",
+        #     "brick-w-hole.vtu",
         #     [777.70784890954064, 777.54021614941144, 0.16763276012921419],
         # ),
         # (
-        #     "tetrahedron.e",
+        #     "tetrahedron.vtu",
         #     [128.31647020288861, 128.3082636471523, 0.0082065557362998032],
         # ),
-        # ("tet.e", [128.31899139655067, 128.30952517579789, 0.0094662207527960365]),
+        # ("tet.vtu", [128.31899139655067, 128.30952517579789, 0.0094662207527960365]),
     ],
 )
 def test_jacobian(filename, control_values):
-    filename = download_mesh(filename)
+    filename = this_dir / "meshes" / filename
     mu = 1.0e-2
 
     mesh = meshplex.read(filename)
@@ -217,40 +198,41 @@ def test_jacobian(filename, control_values):
     phi = numpy.full(num_unknowns, 1j, dtype=complex)
     val = numpy.vdot(phi, mesh.control_volumes * (J * phi)).real
     assert abs(control_values[2] - val) < tol
-    return
 
 
 @pytest.mark.parametrize(
     "filename, control_values",
     [
         (
-            "rectanglesmall.e",
+            "rectanglesmall.vtu",
             [0.50126061034211067, 0.24749434381636057, 0.12373710977782607],
         ),
-        ("pacman.e", [0.71366475047893463, 0.12552206259336218, 0.055859319123267033]),
+        (
+            "pacman.vtu",
+            [0.71366475047893463, 0.12552206259336218, 0.055859319123267033],
+        ),
         # Geometric ce_ratios
         (
-            "cubesmall.e",
+            "cubesmall.vtu",
             [0.00012499993489764605, 4.419415080700124e-05, 1.5624991863028015e-05],
         ),
         (
-            "brick-w-hole.e",
+            "brick-w-hole.vtu",
             [1.8317481239998066, 0.15696030933066502, 0.029179895038465554],
         ),
         # Algebraic ce_ratios
         # (
-        #     "cubesmall.e",
+        #     "cubesmall.vtu",
         #     [8.3541623156163313e-05, 2.9536515963905867e-05, 1.0468744547749431e-05],
         # ),
         # (
-        #     "brick-w-hole.e",
+        #     "brick-w-hole.vtu",
         #     [1.8084716102419285, 0.15654267585120338, 0.03074423493622647],
         # ),
     ],
 )
 def test_f(filename, control_values):
-    filename = download_mesh(filename)
-    mesh = meshplex.read(filename)
+    mesh = meshplex.read(this_dir / "meshes" / filename)
 
     mu = 1.0e-2
     V = -1.0
@@ -259,7 +241,7 @@ def test_f(filename, control_values):
     keo = pyfvm.get_fvm_matrix(mesh, edge_kernels=[Energy(mu)])
 
     # compute the Ginzburg-Landau residual
-    m2 = meshio.read(filename)
+    m2 = meshio.read(this_dir / "meshes" / filename)
     psi = m2.point_data["psi"][:, 0] + 1j * m2.point_data["psi"][:, 1]
     cv = mesh.control_volumes
     # One divides by the control volumes here. No idea why this has been done in pynosh.
@@ -285,4 +267,3 @@ def test_f(filename, control_values):
         numpy.linalg.norm(r.imag, ord=numpy.inf),
     )
     assert abs(control_values[2] - alpha) < tol
-    return
